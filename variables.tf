97a5b490dccb07c4575f4bb650358a24d3b8510b
Sujay Pillai <sujay.pillai@petronas.com>
2024-08-22 22:31:54 +0800

    Remove AKs

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "location" {
   description = "The location of the resource"
   type        = string
+<<<<<<< HEAD
   default     = "eastus"
 }
+=======
+  default     = "southeastasia"
+}
+>>>>>>> 68694ed (Initial terraform project)
